9f5844b9b66dc1483ff90600ecfc4f3969d3a9f3
romanskadiyana <romanskadiyana@gmail.com>
2022-09-01 16:00:52 +0300

    Projects - Exercise - DataTypesAndVariables - 27.05.2022

---
+++ Exercise - DataTypesAndVariables - 27.05.2022/03Elevator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //03. Elevator
+            //Calculate how many courses will be needed to elevate n persons by using an elevator of the capacity of p persons. The input holds two lines: the number of people n and the capacity p of the elevator.
+
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int capacityOfTheElevator = int.Parse(Console.ReadLine());
+
+            double withReminderCourses = (double)numberOfPeople / capacityOfTheElevator;
+
+            Console.WriteLine(Math.Ceiling(withReminderCourses));
+        }
+    }
+}
---
+++ Exercise - DataTypesAndVariables - 27.05.2022/Exercise - DataTypesAndVariables - 27.05.2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise___DataTypesAndVariables___27._05._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
