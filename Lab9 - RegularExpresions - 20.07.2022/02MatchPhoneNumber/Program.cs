6904e89d93676721e4a1b3088ea82a575a2f938e
romanskadiyana <romanskadiyana@gmail.com>
2023-01-11 12:49:16 +0200

    Projects - Lab9 - RegularExpresions - 20.07.2022.

---
+++ Lab9 - RegularExpresions - 20.07.2022/02MatchPhoneNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //02. Match Phone Number
+            //Create a regular expression to match a valid phone number from Sofia. After you find all valid phones, print them on the console, separated by a comma and a space ", ".
+            //            Compose the Regular Expression
+            //A valid number has the following characteristics:
+            //    • It starts with "+359"
+            //    • Then, it is followed by the area code(always 2)
+            //    • After that, it's followed by the number itself:
+            //        ◦ The number consists of 7 digits(separated into two groups of 3 and 4 digits  respectively).
+            //    • The different parts are separated by either a space or a hyphen('-').
+            //You can use the following RegEx properties to help with the matching: 
+            //    • Use quantifiers to match a specific number of digits
+            //    • Use a capturing group to make sure the delimiter is only one of the allowed characters(space   or         hyphen)   and not a combination of both(e.g. +359 2 - 111 111 has mixed delimiters, it  is    invalid).   Use a     group   backreference to achieve this.
+            //    • Add a word boundary at the end of the match to avoid partial matches(the last example on the       right-    hand   side).
+            //    • Ensure that before the '+' sign there is either a space or the beginning of the string.
+            //You can use the following table of values to test your RegEx against:
+
+            Regex regex = new Regex(@"\+359(?<separator>[ \-])2\k<separator>[0-9]{3}\k<separator>[0-9]{4}\b");
+
+            MatchCollection phoneMaches = regex.Matches(Console.ReadLine());
+
+            string[] matchedPhones = phoneMaches.Cast<Match>().Select(x=>x.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
---
+++ Lab9 - RegularExpresions - 20.07.2022/03MatchDates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //03. Match Dates
+            //Write a program, which matches a date in the format "dd{separator}MMM{separator}yyyy". Use named capturing groups in your regular expression.
+            //            Compose the Regular Expression
+            //Every valid date has the following characteristics:
+            //    • Always starts with two digits, followed by a separator.
+            //    • After that, it has one uppercase and two lowercase letters(e.g.Jan, Mar).
+            //    • After that, it has a separator and exactly 4 digits(for the year).
+            //    • The separator could be either of three things: a period('. '), a hyphen('-') or a forward -     slash      ('/').
+            //    • The separator needs to be the same for the whole date(e.g. 13.03.2016 is valid, 13.03 / 2016   is          NOT).Use  a group backreference to check for this.
+            //You can follow the table below to help with composing your RegEx:
+
+
+            Regex regex = new Regex(@"(?<day>[0-9]{2})(?<separator>[\.\-\/])(?<month>[A-Z][a-z]{2})\k<separator>(?<year>[0-9]{4})");
+
+            MatchCollection seekMatches = regex.Matches(Console.ReadLine());
+
+            foreach (Match match in seekMatches)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");            
+            }
+
+
+        }
+    }
+}
